abad42d760112cf41eb2493458ff90b2807591a3
dhirenmathur <dhiru17398@gmail.com>
2020-10-15 08:50:39 +0530

    force 1.8

--- src/main/java/com/whisper/poc/WhisperApplication.java
+++ src/main/java/com/whisper/poc/WhisperApplication.java
@@ -2,7 +2,6 @@ package com.whisper.poc;
 
 import com.whisper.server.controller.LoginController;
 import com.whisper.server.controller.UserController;
-import com.whisper.server.service.UserService;
 import config.WhisperConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
