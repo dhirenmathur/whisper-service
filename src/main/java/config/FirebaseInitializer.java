0a37a4b98503de13c5046c3199142e2c0bfe15fe
dhirenmathur <dhiru17398@gmail.com>
2020-10-15 09:06:41 +0530

    oops

--- src/main/java/config/FirebaseInitializer.java
+++ src/main/java/config/FirebaseInitializer.java
@@ -16,11 +16,11 @@ public class FirebaseInitializer {
     @PostConstruct
     public void initialize() throws IOException {
 
-        FileInputStream serviceAccount = new FileInputStream("./src/main/service_account_pk.json");
+        //FileInputStream serviceAccount = new FileInputStream("./src/main/service_account_pk.json");
 
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                //.setCredentials(GoogleCredentials.getApplicationDefault())
+                .setCredentials(GoogleCredentials.getApplicationDefault())
                 .setDatabaseUrl("https://whisper-db.firebaseio.com")
                 .build();
         FirebaseApp.initializeApp(options);
